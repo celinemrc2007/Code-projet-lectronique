6f4a5d4eef37404705f6cd8437bdd415ce1a2045
Céline <celine.marco92@gmail.com>
2025-12-12 19:17:44 +0100

    Add function declarations for emission and reception

--- emission_reception.h
+++ emission_reception.h
@@ -1,1 +1,5 @@
-
+#include <Arduino.h>
+void emission_receptionSetup (int canal);
+void emettreMessage (char *pseudo_envoye, int *frequence, char *message, int *importance);
+bool recevoirMessage ( char *pseudo_envoye, int *frequence, char *message, int *importance);
+void changerCanal (int canal);
